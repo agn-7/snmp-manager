f7ea793617d7c63addc33018f76e8302c6f0f66a
benyamin_7 <benyamin@infravision.ir>
2018-11-04 11:20:26 +0330

    init __main__

---
+++ __main__.py
@@ -0,0 +1,32 @@
+from threading import Thread
+
+from zmq_listener import Getter
+from collector import BatteryMonitoringReader
+
+__author__ = 'aGn'
+__copyright__ = "Copyright 2018, Planet Earth"
+
+
+class SNMP(Getter):
+    def __init__(self):
+        super().__init__()
+
+    def producer(self):
+        # thread_ = Thread(target=self.listen)
+        thread_ = Thread(target=self.always_listen, args=('SUB',))
+        thread_.daemon = True
+        thread_.start()
+
+        BatteryMonitoringReader().run_forever()
+
+        thread_.join()
+
+
+if __name__ == "__main__":
+    print('Start')
+
+    try:
+        SNMP().producer()
+
+    except KeyboardInterrupt:
+        pass
