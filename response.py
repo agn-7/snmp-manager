e08a14d49ec84785fd962b0ad23effeb6dd8bd3b
benyamin_7 <benyamin@infravision.ir>
2018-11-04 11:46:56 +0330

    init response

---
+++ response.py
@@ -0,0 +1,55 @@
+import zmq
+
+from datetime import datetime
+from time import sleep
+from logger import Logging
+
+__author__ = 'aGn'
+__copyright__ = "Copyright 2018, Planet Earth"
+
+logger = Logging().sentry_logger()
+
+
+class Response(object):
+    def __init__(self):
+        # self.pub_socket = self.create_pub_socket()
+        pass
+
+    @staticmethod
+    def create_pub_socket(ip, port):
+        context = zmq.Context()
+        socket = context.socket(zmq.PUB)
+        zmq_address = "tcp://{}:{}".format(ip, port)
+        socket.connect(zmq_address)
+
+        return socket
+
+    def publish(self, config, *, server_ip, pipeline_ip, pipeline_port, **kwargs):
+        """
+        Packing Json file in order to sending on ZMQ pipeline.
+        :param config: B.M received config.
+        :param server_ip: Server_IP
+        :param pipeline_ip: Pipeline_IP
+        :param pipeline_port: Pipeline_IP
+        :param kwargs: Battery values result.
+        :return:
+        """
+        for key, val in kwargs.items():
+            result = {
+                'data': {key: val},
+                'module': config._name,
+                'time': datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+                'station': 'Battery-Monitoring',
+                'tags': config._meta_data
+            }
+
+            # print(result)  # TODO :: make it to the logger if is necessary.
+            if pipeline_ip != '127.0.0.1':
+                sock = self.create_pub_socket(pipeline_ip, pipeline_port)
+                sock.send_json(result, 0)
+
+            else:
+                sock = self.create_pub_socket(server_ip, pipeline_port)
+                sock.send_json(result, 0)
+
+            # sleep()  # TODO :: maybe need a bit sleeping time.
