23217341f163468af12fc5da1e5fdc61d3f62af6
benyamin_7 <benyamin@infravision.ir>
2018-11-04 10:38:19 +0330

    add logger file

---
+++ logger.py
@@ -0,0 +1,15 @@
+from raven import Client
+
+__author__ = 'aGn'
+__copyright__ = "Copyright 2018, Planet Earth"
+
+
+class Logging(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def sentry_logger():
+        return Client(
+            'http://3bb6148d072c4b3ba3dd1415a68cdd3d:0dbf16ce064a4d10bf24ebbe65eb9d3f@192.168.1.131:9000/20'
+        )
