8621d1d3b5283412a1d8b68a2b271ad29db8d2f1
benyamin_7 <benyamin@infravision.ir>
2018-11-12 13:25:02 +0330

    Designing an OOP pattern for response to the influx pipeline and redis pipeline.

---
+++ scripts/influx_pipeline.py
@@ -0,0 +1,66 @@
+import zmq
+
+from datetime import datetime
+from time import sleep
+from pprint import pprint
+
+from .logger import Logging
+from scripts.response import ResponseAbstract
+
+
+__author__ = 'aGn'
+__copyright__ = "Copyright 2018, Planet Earth"
+
+logger = Logging().sentry_logger()
+
+
+class InfluxPipeline(ResponseAbstract):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def create_pub_socket(ip, port):
+        context = zmq.Context()
+        socket = context.socket(zmq.PUB)
+        zmq_address = "tcp://{}:{}".format(ip, port)
+        socket.connect(zmq_address)
+
+        return socket
+
+    def publish(
+            self, module, meta_data, *,
+            server_ip='127.0.0.1', pipeline_ip='127.0.0.1', pipeline_port=9001,
+            **kwargs
+    ):
+        """
+        Packing Json file in order to sending on ZMQ pipeline.
+        :param config: B.M received config.
+        :param module:
+        :param meta_data:
+        :param server_ip: Server_IP
+        :param pipeline_ip: Pipeline_IP
+        :param pipeline_port: Pipeline_IP
+        :param kwargs: Battery values result.
+        :return:
+        """
+        for name, data in kwargs.items():
+            result = {
+                'data': {name: data},
+                'module': module,
+                'time': datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+                'station': 'SNMP',
+                'tags': meta_data
+            }
+
+            pprint(result)  # TODO :: make it to the logger if is necessary.
+            if pipeline_ip != '127.0.0.1':
+                sock = self.create_pub_socket(pipeline_ip, pipeline_port)
+                # sock.send_json(result, 0)
+                sock.send_json(result, flags=zmq.NOBLOCK)  # TODO
+
+            else:
+                sock = self.create_pub_socket(server_ip, pipeline_port)
+                # sock.send_json(result, 0)
+                sock.send_json(result, flags=zmq.NOBLOCK)  # TODO
+
+            # sleep()  # TODO :: maybe need a bit sleeping time.
--- scripts/response.py
+++ scripts/response.py
@@ -1,64 +1,47 @@
-import zmq
+import abc
 
-from datetime import datetime
-from time import sleep
+from abc import abstractmethod
+
 from .logger import Logging
-from pprint import pprint
+from scripts.redis_pipeline import RedisPipeline
+from scripts.influx_pipeline import InfluxPipeline
+
 
 __author__ = 'aGn'
 __copyright__ = "Copyright 2018, Planet Earth"
 
 logger = Logging().sentry_logger()
 
+PIPELINE = [RedisPipeline(), InfluxPipeline()]  # TODO :: Make it dynamically.
 
-class Response(object):
+
+class ResponseAbstract(object):
+    __metaclass__ = abc.ABCMeta
+
     def __init__(self):
-        # self.pub_socket = self.create_pub_socket()
-        pass
+        return
 
-    @staticmethod
-    def create_pub_socket(ip, port):
-        context = zmq.Context()
-        socket = context.socket(zmq.PUB)
-        zmq_address = "tcp://{}:{}".format(ip, port)
-        socket.connect(zmq_address)
-
-        return socket
-
+    @abstractmethod
     def publish(
             self, module, meta_data, *,
             server_ip='127.0.0.1', pipeline_ip='127.0.0.1', pipeline_port=9001,
+            **kwargs):
+        pass
+
+
+class Response(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def publish(
+            module, meta_data, *,
+            server_ip='127.0.0.1', pipeline_ip='127.0.0.1', pipeline_port=9001,
             **kwargs
     ):
-        """
-        Packing Json file in order to sending on ZMQ pipeline.
-        :param config: B.M received config.
-        :param module:
-        :param meta_data:
-        :param server_ip: Server_IP
-        :param pipeline_ip: Pipeline_IP
-        :param pipeline_port: Pipeline_IP
-        :param kwargs: Battery values result.
-        :return:
-        """
-        for name, data in kwargs.items():
-            result = {
-                'data': {name: data},
-                'module': module,
-                'time': datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
-                'station': 'SNMP',
-                'tags': meta_data
-            }
-
-            pprint(result)  # TODO :: make it to the logger if is necessary.
-            if pipeline_ip != '127.0.0.1':
-                sock = self.create_pub_socket(pipeline_ip, pipeline_port)
-                # sock.send_json(result, 0)
-                sock.send_json(result, flags=zmq.NOBLOCK)  # TODO
-
-            else:
-                sock = self.create_pub_socket(server_ip, pipeline_port)
-                # sock.send_json(result, 0)
-                sock.send_json(result, flags=zmq.NOBLOCK)  # TODO
-
-            # sleep()  # TODO :: maybe need a bit sleeping time.
+        for pipe in PIPELINE:
+            pipe.publish(
+                module, meta_data,
+                server_ip=server_ip, pipeline_ip=pipeline_ip, pipeline_port=pipeline_port,
+                **kwargs
+            )
