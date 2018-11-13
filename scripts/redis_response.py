32f5ea167a7c32c44ec588d4cad8a11b97b55219
benyamin_7 <benyamin@infravision.ir>
2018-11-13 12:55:35 +0330

    resolve the zmq.PUB problem in redis response

--- scripts/redis_response.py
+++ scripts/redis_response.py
@@ -1,4 +1,5 @@
 import zmq
+import time
 
 from datetime import datetime
 from pprint import pprint
@@ -14,14 +15,21 @@ logger = Logging().sentry_logger()
 
 
 class RedisResponse(ResponseAbstract):
-    @staticmethod
-    def create_pub_socket(ip, port):
+    def __init__(self, server_ip, pipeline_ip, pipeline_port):
+        super().__init__()
+        self.socket = None
+        self.server_ip = server_ip
+        self.pipeline_ip = pipeline_ip
+        self.pipeline_port = pipeline_port
+
+    # def create_pub_socket(self, ip, port):
         context = zmq.Context()
         socket = context.socket(zmq.PUB)
-        zmq_address = "tcp://{}:{}".format(ip, port)
+        zmq_address = "tcp://{}:{}".format(self.pipeline_ip, self.pipeline_port)
         socket.connect(zmq_address)
+        self.socket = socket
 
-        return socket
+        # return socket
 
     def publish(
             self, module, meta_data,
@@ -49,13 +57,26 @@ class RedisResponse(ResponseAbstract):
 
             pprint(result)  # TODO :: make it to the logger if is necessary.
             if self.pipeline_ip != '127.0.0.1':
-                sock = self.create_pub_socket(self.pipeline_ip, self.pipeline_port)
-                # sock.send_json(result, 0)
-                sock.send_json(result, flags=zmq.NOBLOCK)  # TODO
+                # sock = self.create_pub_socket(self.pipeline_ip, self.pipeline_port)
+                # sock.send_json(result, flags=0)
+                try:
+                    self.socket.send_json(result, flags=zmq.NOBLOCK)  # TODO
+                except zmq.ZMQError as exc:
+                    print('Space if full >> {}'.format(exc))
+                    time.sleep(1)
 
             else:
-                sock = self.create_pub_socket(self.server_ip, self.pipeline_port)
-                # sock.send_json(result, 0)
-                sock.send_json(result, flags=zmq.NOBLOCK)  # TODO
+                print(self.pipeline_port)
+                # self.create_pub_socket(self.server_ip, self.pipeline_port)
 
-                # sleep()  # TODO :: maybe need a bit sleeping time.
+                if self.socket is not None:
+                    # sock.send_json(result, flags=0)
+                    try:
+                        self.socket.send_json(result, flags=zmq.NOBLOCK)  # TODO
+                    except zmq.ZMQError as exc:
+                        print('Space if full >> {}'.format(exc))
+                        time.sleep(1)
+
+                else:
+                    print('There is not any socket')
+                # time.sleep(1e-1)  # TODO :: maybe need a bit sleeping time.
