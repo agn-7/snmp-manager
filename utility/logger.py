d060b99514c58bb922be306d4290ee6f42355cde
benyamin_7 <benyamin@infravision.ir>
2018-12-18 12:51:16 +0330

    trying to add some cases to the logs

--- utility/logger.py
+++ utility/logger.py
@@ -3,6 +3,44 @@ from raven import Client
 __author__ = 'aGn'
 __copyright__ = "Copyright 2018, Planet Earth"
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+
+    'formatters': {
+        'console': {
+            'format': '[%(asctime)s][%(levelname)s] %(name)s '
+                      '%(filename)s:%(funcName)s:%(lineno)d | %(message)s',
+            'datefmt': '%H:%M:%S',
+            },
+        },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+            },
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.handlers.logging.SentryHandler',
+            'dsn': 'http://e627c6d4737d4970a095e2b5c95a0269:7fca1cacbfdd4d2298618663668faba0@192.168.1.136/2',
+            },
+        },
+
+    'loggers': {
+        '': {
+            'handlers': ['console', 'sentry'],
+            'level': 'DEBUG',
+            'propagate': False,
+            },
+        'your_app': {  # TODO
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
+
 
 class Logging(object):
     def __init__(self):
