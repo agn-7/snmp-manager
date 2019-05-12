705933091f8b0a59505946dea40cfaf376cb5c95
benyamin_7 <benyamin@infravision.ir>
2019-05-12 11:32:06 +0430

    update logging

--- collect/collector.py
+++ collect/collector.py
@@ -65,7 +65,9 @@ class SNMPReader(object):
             )
 
             if error_indication:
-                logger.captureMessage(error_indication)
+                str_error = f"tag_name: {name} - OID: {oid} - IP: {address} \n " \
+                            f"{error_indication}"
+                logger.captureMessage(str_error)
                 data = -8555
 
             elif error_status:
