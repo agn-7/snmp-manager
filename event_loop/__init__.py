e947bfbc60174b8f7db1c6a0c5511f3fbabcc948
benyamin jafari <benyaminjmf@gmail.com>
2020-07-31 16:46:22 +0430

    update setup

--- event_loop/__init__.py
+++ event_loop/__init__.py
@@ -1,5 +1,5 @@
 from .event_loop import *
 
-__version__ = '1.0.0-rc1'
+__version__ = '1.0.0rc3'
 __author__ = 'aGn'
 __copyright__ = "Copyright 2018, Planet Earth"
--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import sys
+
 from setuptools import setup
+from event_loop import __version__
+
+
+if sys.version_info[0] < 3:
+    with open('README.md') as f:
+        long_description = f.read()
+else:
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
 
 
 setup(
     name='simple_snmp_collector',
-    version='1.0.0-rc1',
+    version=__version__,
     description='SNMP collector through an asyncio loop',
+    long_description=long_description,
     url='https://github.com/agn-7/simple-snmp-collector',
     author='agn-7',
     author_email='benyaminjmf@gmail.com',
@@ -25,5 +37,8 @@ setup(
         'easydict',
         'pysnmp==4.4.9',
         'async-timeout'
-    ]
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
 )
