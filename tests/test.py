b0b3c9cefab172acad193e6348f31bb57eab7581
benyamin jafari <benyaminjmf@gmail.com>
2022-02-12 20:20:01 +0330

    update unit test

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,10 @@ from snmp_collector.event_loop import EventLoop
 
 
 class TestCase:
-    @pytest.mark.parametrize("test_case", [])
+    @pytest.mark.parametrize("test_case", ['none'])
     def test_events(self, test_case):
         assert EventLoop().run_once()
+
+
+if __name__ == "__main__":
+    assert EventLoop().run_once()
