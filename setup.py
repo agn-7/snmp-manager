441452ffbac9544ebf4a91a187b68becddb7c0cb
benyamin jafari <benyaminjmf@gmail.com>
2022-02-14 16:45:36 +0330

    update setup files

--- setup.py
+++ setup.py
@@ -14,30 +14,7 @@ else:
 
 setup(
     version=__version__,
-    description='SNMP collector through an asyncio loop',
     long_description=long_description,
     packages=find_packages(),
-    keywords=[
-        'snmp',
-        'snmp-collector',
-        'snmp-manager'
-        'asyncio',
-        'python3',
-        'python',
-        'docker',
-        'docker-compose'
-    ],
     download_url=f'https://github.com/agn-7/snmp-manager/archive/{__version__}.zip',
-    install_requires=[
-        'pyserial>=3',
-        'easydict>=1',
-        'pysnmp>=4',
-        'async-timeout>=3'
-    ],
-    # classifiers=[
-    #     'Programming Language :: Python :: 3.6',
-    #     'Programming Language :: Python :: 3.7',
-    #     'Programming Language :: Python :: 3.8',
-    #     'Programming Language :: Python :: 3.9'
-    # ],
 )
