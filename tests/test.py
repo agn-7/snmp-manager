d1a92bbbda8a4377ebae0f4cc7ffe71f4d3d8159
agn-7 <benyaminjmf@gmail.com>
2022-02-21 09:58:30 +0330

    extend unit test

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,34 @@
 import pytest
+import asyncio
+
+from interruptingcow import timeout
 
 from snmp_collector.event_loop import EventLoop
 
 
 class TestCase:
+    @pytest.mark.timeout(10)
+    def endless(self):
+        EventLoop().run_forever()
+
     @pytest.mark.parametrize("test_case", ['none'])
-    def test_events(self, test_case):
+    def test_events(self, test_case: list):
         assert EventLoop().run_once()
 
+    @pytest.mark.parametrize("test_case", ['none'])
+    def test_events2(self, test_case: list):
+        try:
+            with timeout(10, exception=asyncio.CancelledError):
+                EventLoop().run_forever()
+                assert False
+        except:
+            assert True
 
 if __name__ == "__main__":
     assert EventLoop().run_once()
+    try:
+        with timeout(5, exception=asyncio.CancelledError):
+            EventLoop().run_forever()
+            assert False
+    except:
+        assert True
