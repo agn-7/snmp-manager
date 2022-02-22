48de59616e8ced8bc122ac26873c3dfe146f1c12
agn-7 <benyaminjmf@gmail.com>
2022-02-22 15:24:55 +0330

    lint black

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from snmp_collector import __version__
 
 
 if sys.version_info[0] < 3:
-    with open('README.rst') as f:
+    with open("README.rst") as f:
         long_description = f.read()
 else:
-    with open('README.rst', encoding='utf-8') as f:
+    with open("README.rst", encoding="utf-8") as f:
         long_description = f.read()
 
 
@@ -16,5 +16,5 @@ setup(
     version=__version__,
     long_description=long_description,
     packages=find_packages(),
-    download_url=f'https://github.com/agn-7/snmp-manager/archive/{__version__}.zip',
+    download_url=f"https://github.com/agn-7/snmp-manager/archive/{__version__}.zip",
 )
